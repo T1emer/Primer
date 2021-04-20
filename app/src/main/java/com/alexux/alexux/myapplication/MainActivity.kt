57d495f470d67c0db40a3d966378d88f605527f0
T1emer <sashareshetovalex@gmail.com>
2021-04-20 14:19:50 +0700

    Sasha

--- app/src/main/java/com/alexux/alexux/myapplication/MainActivity.kt
+++ app/src/main/java/com/alexux/alexux/myapplication/MainActivity.kt
@@ -25,6 +25,7 @@ class MainActivity : AppCompatActivity() {
  findViewById<Button>(R.id.button).setOnClickListener {
 
 
+
      val userLogin = findViewById<EditText>(R.id.login).text.toString()
 
      if (userLogin == trueLogin) {
